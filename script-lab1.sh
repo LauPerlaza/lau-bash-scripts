688eb247bb9b1b8c3f96483429e2a9e80941a4de
lauperlaza <laura.perlaza2907@gmail.com>
2023-03-06 21:25:56 -0500

    algunos cambios sugeridos por Daniel

--- script-lab1.sh
+++ script-lab1.sh
@@ -2,8 +2,11 @@
 #script para crear un inventario del networking de la consola del cliente
 #describe las instancia
 nombre_archivo="inventario-aws-$(date '+%Y-%m-%d').json" 
-echo $nombre_archivo
+echo " el archivo a crear es $nombre_archivo "
 aws ec2 describe-instances > $nombre_archivo
 #luego crea un archivo y lo guarda
 aws s3 cp $nombre_archivo s3://bucket-data-lab/
 #copia el archivo en aws s3
+##Eliminar archivo local
+rm -f $nombre_archivo
+echo  " archivo $nombre_archivo eliminado "
